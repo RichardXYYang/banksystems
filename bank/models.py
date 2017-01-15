125554d294d8e13c6feb6e6a086363298fbc5afe
RichardXYYang <yxiaoyun2002@yahoo.com>
2017-01-14 20:43:25 -0600

    test
    bug

--- bank/models.py
+++ bank/models.py
@@ -14,3 +14,7 @@ class Musician(models.Model):
 class LoginForm(forms.Form):
 	username = forms.CharField(max_length=100)
 	password = forms.CharField(widget=forms.PasswordInput())
+
+
+
+#jfklwfjkdsajlkasdjfkldsjfdklasjfdlaksjfko
